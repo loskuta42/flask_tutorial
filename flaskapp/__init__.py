339c5a936c1730ea9b991bed31dc7267b1ff18d7
Alexey <loskuta42@yandex.ru>
2022-07-25 18:04:40 +0300

    finish DB, begin views

--- flaskapp/__init__.py
+++ flaskapp/__init__.py
@@ -8,7 +8,7 @@ def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+        DATABASE=os.path.join(app.instance_path, 'flaskapp.sqlite'),
     )
 
     if test_config is None:
@@ -28,5 +28,9 @@ def create_app(test_config=None):
     @app.route('/wakeup')
     def wake_up():
         return 'Wake up, Neo...'
+    
+    from . import db
+    db.init_app(app)
+
 
     return app
