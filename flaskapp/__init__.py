6e01793d6605762fed868b6b9834984e756215ed
Alexey <loskuta42@yandex.ru>
2022-07-28 18:05:43 +0300

    UPD/view and template for register

--- flaskapp/__init__.py
+++ flaskapp/__init__.py
@@ -2,6 +2,8 @@ import os
 
 from flask import Flask
 
+from . import db, auth
+
 
 def create_app(test_config=None):
     # создаем и настраиваем приложение
@@ -29,8 +31,9 @@ def create_app(test_config=None):
     def wake_up():
         return 'Wake up, Neo...'
     
-    from . import db
     db.init_app(app)
+    
+    app.register_blueprint(auth.bp)
 
 
     return app
--- flaskapp/auth.py
+++ flaskapp/auth.py
@@ -8,3 +8,32 @@ from werkzeug.security import check_password_hash, generate_password_hash
 from flaskapp.db import get_db
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
+
+@bp.route('/register', methods=('GET', 'POST'))
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db()
+        error = None
+
+        if not username:
+            error = 'Имя пользователя является обязательным полем.'
+        elif not password:
+            error = 'Пароль является обязательным полем.'
+
+        if error is None:
+            try:
+                db.execute(
+                    "INSERT INTO user (username, password) VALUES (?, ?)",
+                    (username, generate_password_hash(password)),
+                )
+                db.commit()
+            except db.IntegrityError:
+                error = f"Пользователь {username} уже зарегистрирован."
+            else:
+                return redirect(url_for("auth.login"))
+
+        flash(error)
+
+    return render_template('auth/register.html')
