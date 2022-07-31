b5c7d19ddcf204011aa61bf9aab7854a424e343f
loskuta42 <loskuta42@yandex.ru>
2022-07-31 20:56:09 +0300

    UPD/add and finish blog blueprint but not save g object

--- flaskapp/auth.py
+++ flaskapp/auth.py
@@ -62,3 +62,13 @@ def login():
         flash(error)
 
     return render_template('auth/login.html')
+
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for('auth.login'))
+
+        return view(**kwargs)
+
+    return wrapped_view
